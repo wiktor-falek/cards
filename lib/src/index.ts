3c0653b741c9febfd3a3ec8ada7fa93263ae67c8
wiktor-falek <falekwiktor@gmail.com>
2023-08-11 21:54:32 +0200

    add function for getting value of the card

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -42,3 +42,34 @@ class Deck {
 
   draw() {}
 }
+
+function getCardValue(card: Card) {
+  switch (card.rank) {
+    case "Ace":
+      return 14;
+    case "King":
+      return 13;
+    case "Queen":
+      return 12;
+    case "Jack":
+      return 11;
+    case "10":
+      return 10;
+    case "9":
+      return 9;
+    case "8":
+      return 8;
+    case "7":
+      return 7;
+    case "6":
+      return 6;
+    case "5":
+      return 5;
+    case "4":
+      return 4;
+    case "3":
+      return 3;
+    case "2":
+      return 2;
+  }
+}
